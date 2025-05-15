777c72d648f5521b033edb6097886da518472b79
3nd0rf1n <saylen2006@gmail.com>
2025-05-15 16:11:07 +0300

    123

--- achievements.js
+++ achievements.js
@@ -62,3 +62,49 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }, 150);
 });
+
+particlesJS('particles-js',
+  {
+    "particles": {
+      "number": {
+        "value": 60,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.3
+      },
+      "size": {
+        "value": 3
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.2,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 2
+      }
+    },
+    "interactivity": {
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        }
+      }
+    },
+    "retina_detect": true
+  }
+);
