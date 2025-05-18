e26fc23e3391f1b568c2ed7ab4d1eba72f0f0a70
3nd0rf1n <saylen2006@gmail.com>
2025-05-18 17:10:38 +0300

    123

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,7 @@
-const CACHE_NAME = 'my-game-cache-v1';
-const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Кеширование файлов');
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
 });
 
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('Удаление старого кеша', key);
-          return caches.delete(key);
-        }
-      }))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (evt) => {
-  evt.respondWith(
-    caches.match(evt.request).then((response) => {
-      return response || fetch(evt.request);
-    })
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
