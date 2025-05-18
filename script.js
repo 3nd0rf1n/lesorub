cf304fc7b9e6b222f9c17829e0d2b3e655b0b5d6
3nd0rf1n <saylen2006@gmail.com>
2025-05-18 16:52:59 +0300

    123

--- script.js
+++ script.js
@@ -316,31 +316,3 @@ window.addEventListener('storage', (e) => {
   if (e.key === 'graphicsQuality') applyGraphicsQuality();
 });
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('ServiceWorker зарегистрирован:', registration);
-    }).catch(err => {
-      console.log('Ошибка регистрации ServiceWorker:', err);
-    });
-  });
-}
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  // Здесь покажи кнопку "Установить"
-  showInstallButton();
-
-  installButton.addEventListener('click', () => {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('Пользователь установил приложение');
-      }
-      deferredPrompt = null;
-    });
-  });
-});
